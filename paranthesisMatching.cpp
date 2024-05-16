48c1a88ec6c214fbfd20d75b9d8fd27e7ec3e401
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-05-16 14:02:35 +0530

    Add files via upload

---
+++ paranthesisMatching.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    char data;
+    struct Node *next;
+} *top = NULL;
+
+void push(char x){
+    struct Node *t;
+    t = new Node;
+
+    if(t==NULL)
+        cout << "stack is full" << endl;
+    else{
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+char pop(){
+    struct Node *t;
+    char x =-1;
+
+    if(top==NULL)
+        cout << "stack is empty" << endl;
+    else{
+        t = top;
+        top = top->next;
+        x = t->data;
+        delete t;
+    }
+    return x;
+}
+
+void Display(){
+    struct Node *p;
+    p = top;
+    while (p!=NULL)
+    {
+        cout << p->data;
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int isBalanced(char *exp)
+{
+    int i; 
+    for ( i = 0; exp[i]!='\0'; i++)
+    {
+        if(exp[i]=='(')
+            push(exp[i]);
+        else if(exp[i]==')'){
+            if(top==NULL)
+                return 0;
+            pop();
+        }
+    }
+    if(top==NULL)
+        return 1;
+    else
+        return 0;
+}
+
+
+int main(){
+    char *exp = "((a+b)*(c-d))";
+    cout<<isBalanced(exp);
+    return 0;
+}
